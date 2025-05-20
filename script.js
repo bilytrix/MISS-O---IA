29fca5ff8fa6e0377404bd6b7d814de590121e35
beatriz costa de souza <137806317+bilytrix@users.noreply.github.com>
2025-05-20 07:42:26 -0300

    fg

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
   {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Quais os principais autores do Barroco no Brasil?",
+    alternativas: ["Gregório de Matos", "Bento Teixeira e Manuel Botelho de Oliveira"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Quais os países que têm a maior e a menor expectativa de vida do mundo?",
+    alternativas: ["Japão", "Serra Leoa"],
      },
   {
-    enunciado: "Pergunta 3",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Quais as duas datas que são comemoradas em novembro?",
+    alternativas: ["Proclamação da República", "Dia Nacional da Consciência Negra"],
      },
   {
-    enunciado: "Pergunta 4",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Qual a altura da rede de vôlei nos jogos masculino e feminino?",
+    alternativas: ["2,43", "2,24"],
      },
   {
-    enunciado: "Pergunta 5",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?",
+    alternativas: ["Entre 4 a 6 litros", "São retirados 450 mililitros"],
   },
 ];
